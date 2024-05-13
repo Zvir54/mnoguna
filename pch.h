5dd261ebe90a2670ef02873d8c1aacd63c4b9421
Ihor222 <98274099+Ihor222@users.noreply.github.com>
2024-05-13 20:43:21 +0300

    Update pch.h

--- pch.h
+++ pch.h
@@ -75,4 +75,158 @@ Set<T>::Set(T arr_)
     }
 }
 
+template<typename T>
+Set<T>::Set()
+{
+ position= 0;
+}
+template<typename T>
+void Set<T>::add(T x) // додаєм елемент .
+{
+    if (position != size_t - 1)
+    {
+        bool check = true;
+        for (int j = 0; j < position; ++j)
+        {
+            if (x == arr[j])
+            {
+                check = false;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        if (check)
+        {
+
+            arr[position] = x;
+            position++;
+        }
+    }
+        else
+        {
+            cout << "Set is full, can't add new element \n";
+        }
+}
+
+template<typename T>
+void Set<T>::remove(T x) // вилучаєм елемент
+{
+    for (int i = 0; i < position; ++i)
+    {
+        if (arr[i] == x)
+        {
+            for (int j = i; j < position; ++j)
+            {
+                arr[j] = arr[j + 1];
+            }
+            arr[position] = 0;
+        }
+    }
+position--;
+}
+template<typename T>
+void Set<T>::clear() // Очищуєм весь масив
+{
+  for (int i = 0; i < position; ++i)
+  {
+      arr[i] = 0;
+  }
+}
+template<typename T>
+bool Set<T>::Check(T x) // Перевірка наявності елемента в множині
+{
+  bool check = false;
+  for ( int i = 0; i < position; ++i)
+  {
+      if (arr[i] == x)
+      {
+          check = true;
+          break;
+      }
+  }
+    return check;
+}
+template<typename T>
+int Set<T>::set_size() //Розмір множини
+{
+ return position;  // повертає кількість елементів які вже є в множині
+}
+template<typename T>
+Set<T> Set<T>::union_(Set& S) //Обєднання множин
+{
+  Set un = S;
+  for (int i = 0; i < position; ++i)
+  {
+      if (!un.Check(arr[i]))
+      {
+          if (un.set_size() < size_t)
+          {
+              un.add(arr[i]);
+              un.position++;
+          }
+      }
+  }
+  return un;
+}
+template<typename T>
+Set<T> Set<T>::intersect(Set& S) // Перетин множин
+{
+    Set in;
+    for (int i = 0; i < position; ++i)
+    {
+        for (int j = 0; j < S.position; ++j )
+        {
+            if (arr[i] == S.arr[j])
+            {
+                in.add(arr[i]);
+                break;
+            }
+        }
+    }
+    return in;
+}
+
+
+
+
+
+template<typename T>
+Set<T> Set<T>::difference (Set& S) // Різниця двох множин
+{
+    Set dif;
+    for (int i = 0; i < position; ++i)
+    {
+        bool check = true;
+        for (int j = 0; j < S.position; ++j )
+        {
+            if (arr[i] == S.arr[j])
+            {
+                check = false;
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        if (check)
+        {
+            dif.add(arr[i]);
+            dif.position++;
+        }
+    }
+    return dif;
+}
+template<typename T>
+void Set<T>::print(ostream& os) // вивід в потік
+{
+ for (int i = 0; i < position; ++i)
+ {
+     os << arr[i] << ' ';
+ }
+ os << endl;
+}
 
